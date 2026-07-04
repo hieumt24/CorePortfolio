6e83dc9f2f949f5c93c20c0f05ae0a5ff0d4cc4f
Hieu Mai Trong <hieu.maitrong@nashtechglobal.com>
2026-07-04 10:16:55 +0700

    fix: run ef core database migrations on startup for azure

--- backend/src/CorePortfolio.API/Program.cs
+++ backend/src/CorePortfolio.API/Program.cs
@@ -100,6 +100,8 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
